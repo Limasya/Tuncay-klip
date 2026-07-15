5804a13cf3ec4f88f43f72a1687bf9b5539694fb
babaflex <babaflex3138@gmail.com>
2026-07-15 23:47:56 +0300

    yarak

--- test_cv2.py
+++
@@ -1,7 +0,0 @@
-import sys
-print("A - importing cv2", flush=True)
-import cv2
-print("B - cv2 OK", flush=True)
-print("C - importing stream_capture", flush=True)
-from services.stream_capture import stream_capture
-print("D - stream_capture OK", flush=True)
